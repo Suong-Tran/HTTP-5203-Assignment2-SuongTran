62d3021d9ae6a110b4d9af08934d59c33a4311b3
Suong <starolchen102@gmail.com>
2021-07-27 23:16:52 +0700

    adding a  button in ticket-detail.php to change status into resolved

--- ticket-detail.php
+++ ticket-detail.php
@@ -1,13 +1,19 @@
 <?php
-$contents = $datetime = $status = '';
+
 $xml = simplexml_load_file("xml/tickets.xml");
 
-$xml->asXml('xml/tickets.xml');
+
+$id = "";
 if(isset($_POST['ticketDetail'])){
     $id = $_POST['id'];
 }
+else if(isset($_POST['update-status'])){
+    $id = $_POST['id'];
+    resolved($xml,$id);
+}
+$xml->asXml('xml/tickets.xml');
 
-
+$contents = $datetime = $status = '';
 foreach ($xml->children() as $p) {
     if($p->attributes()->id == $id){
         $datetime = new DateTime($p->messages->message[1]->attributes()->date);
@@ -20,15 +26,36 @@ foreach ($xml->children() as $p) {
     }
     
   }
+
+function resolved($xml,$id){
+    foreach ($xml->children() as $p) {
+        if($p->attributes()->id == $id){
+            $p->attributes()->status = "resolved";
+        }       
+      }
+}
   ?>
   <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Support tickets</title>
-  </head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+</head>
   <body>
-      <h1>Ticket Details</h1>
+      
+        <h1>Ticket Details</h1>
+        <div class="d-flex justify-content-between">
+            <form action="ticket-listing.php" method="post">
+                <input type="submit" class="button btn btn-primary" name="ticket-listing" value="Return"/>
+            </form>
+            <form action="ticket-detail.php" method="post">
+                <input type="hidden" name="id" value=" <?= $id ?> "/>
+                <input type="submit" class="button btn btn-primary" name="update-status" value="Solved"/>
+            </form>
+        </div>
+        
       <p>Ticket number: <?=$id?> </p>
       <p>Open date: <?= $datetime->format('Y-m-d') ?>
       <p>Status: <?= $status?></p>
